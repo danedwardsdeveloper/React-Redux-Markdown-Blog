7148b862fe586c74ca1140791894d82079d4f39b
Dan Edwards <danedwardscreative@gmail.com>
2024-05-17 15:19:43 +0100

    Cleanup store

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,9 @@
-import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import articlesReducer from "../features/articles/articlesSlice";
-// import categoriesReducer from "../features/categories/categoriesSlice";
-// import sessionReducer from "../features/session/sessionSlice";
-// import authorsReducer from "../features/authors/authorsSlice";
 
 export const store = configureStore({
   reducer: {
     articlesSlice: articlesReducer,
-    // categories: categoriesReducer,
-    // session: sessionReducer,
-    // authors: authorsReducer,
   },
   devTools: true,
 });
